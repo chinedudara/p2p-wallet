4f886ff48b1b554427ae121af6cdd8b1b353de3c
Chinedu Okechukwu-Dara <darachinedu@gmail.com>
2022-08-28 08:56:37 +0100

    Setup authentication - Register

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -10,5 +10,31 @@ auth.post('/login', async (req, res) => {
 })
 
 auth.post('/register', async (req, res) => {
+    const { email, username, password } : { email: string, username: string, password: string }= req.body;
+    const result = await prisma.user.findUnique({
+        where: { username }
+    })
     
+    if(result){
+        return res
+        .status(400)
+        .json({
+            success: false,
+            error: "User already exists"
+        })
+    }
+
+    const user = await prisma.user.create({
+        data: {
+            username: username,
+            email: email,
+            password: password
+        }
+    })
+
+    const accessToken = jwt.sign({userId: user.id}, "secret")
+    return res.status(200).json({
+        success: true,
+        accessToken: accessToken
+    })
 })
