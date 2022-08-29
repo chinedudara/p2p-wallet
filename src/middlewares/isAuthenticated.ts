ddc0252970c65f8b3a9aa923d12a78c4398c408b
Chinedu Okechukwu-Dara <darachinedu@gmail.com>
2022-08-29 14:41:02 +0100

    Modified authentication middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,10 +1,20 @@
 import argon2 from "argon2";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, user } from "@prisma/client";
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+
+import { TestReq } from "../../typings/typings";
 
 const prisma = new PrismaClient()
+const AUTH_SECRET: any = process.env.AUTH_SECRET
 
-export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+// interface RequestAuth extends Request{
+//     user: {
+//         id: string
+//     }
+// }
+
+export const isAuthenticated = async (req: TestReq, res: Response, next: NextFunction) => {
     if(!req.headers["authorization"]){
         return res
         .status(400)
@@ -13,8 +23,52 @@ export const isAuthenticated = async (req: Request, res: Response, next: NextFun
             error: "No header provided"
         })
     }
+
+    const authHeader = req.headers["authorization"];
+    const authMethod = authHeader.split(" ")[0];
+    const token = authHeader.split(" ")[1];
     
+    if (!authMethod || !token)
+    return res
+      .status(400)
+      .json({ success: false, error: "Invalid Authorization Header" });
+  if (authMethod !== "Bearer")
+    return res
+      .status(400)
+      .json({ success: false, error: "Invalid Authorization Method" });
+
+  let tokenBody: any;
+
+  try {
+    tokenBody = jwt.verify(token, AUTH_SECRET);
+  } catch {
+    return res
+      .status(400)
+      .json({ success: false, error: "Invalid Token", invalidate: true });
+  }
     
-    
-    next()
+  console.log(tokenBody);
+
+  if (!tokenBody.userId)
+    return res.status(400).json({ success: false, error: "Invalid Token" });
+
+  const user = await prisma.user.findUnique({
+    where: { id: tokenBody.userId },
+    include: { account: true}
+  });
+
+  if (!user)
+    return res
+      .status(400)
+      .json({ success: false, error: "User does not exist" });
+
+  req.user = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    account_number: user.account?.account_number || null,
+    balance: user.account?.balance
+  };
+
+    return next()
 }
