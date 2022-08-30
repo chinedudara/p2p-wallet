018ea11f4f69982a1c62f33d9397e6db63d27979
Chinedu Okechukwu-Dara <darachinedu@gmail.com>
2022-08-30 14:04:35 +0100

    feat: Created config setup & response codes enum

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,17 @@
+import * as dotenv from "dotenv";
+dotenv.config()
+
+export const common_config = {
+    port: process.env.PORT || 3000,
+    debug: process.env.DEBUG || false,
+    tokenSecret: process.env.AUTH_SECRET || "secret",
+}
+
 export const payment_integration = {
-    public_key: process.env.PAYSTACK_PUBLIC_KEY,
-    secret_key: process.env.PAYSTACK_SECRET_KEY,
-    verification_url: 'https://api.paystack.co/transaction/verify/'
+    public_key: process.env.PAYSTACK_PUBLIC_KEY || "",
+    secret_key: process.env.PAYSTACK_SECRET_KEY || "",
+    verification_url: 'https://api.paystack.co/transaction/verify/',
+    payment_url: 'https://api.paystack.co/transaction/initialize'
 }
 
 export const transaction_status = {
